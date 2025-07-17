89dddb26c6c6331504b64597b79e579850cee9d5
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-07-17 18:13:31 -0300

    Creacion del proyecto y las funciones basicas

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+//Variables
+const d = document;
+//Funciones
+let sumar = (a, b) => a + b;
+let restar = (a, b) => a - b;
+let multiplicar = (a, b) => a * b;
+let dividir = (a, b) => a / b;
+//Ejecucion
