1e4ab7af3ac3ca0ef00fab5f1a432a1d597529af
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-07-20 07:20:03 -0300

    Termine el proyecto
    Aunque no hice muchos commits y empece de cero varias veces
    a partir de esto funcional voy a ir creando ramas
    con nuevas caracteristicas

--- script.js
+++ script.js
@@ -1,32 +1,127 @@
-console.log("Hello World");
 //Variables
 const d = document;
-let numUno;
-let numDos;
-let operador;
+
+let numero = "";
+let auxiliar = "";
+let operador = "";
+
+let contOperador = 0;
+let resultado = "";
+
+const $display = d.querySelector("[data-display]");
+
 //Funciones
 let sumar = (a, b) => a + b;
 let restar = (a, b) => a - b;
 let multiplicar = (a, b) => a * b;
 let dividir = (a, b) => a / b;
 
-let operate = (numUno, operador, numDos) => {
+let operate = (primerTermino, segundoTermino, operador) => {
   switch (operador) {
     case "+":
-      return sumar(numUno, numDos);
+      return sumar(primerTermino, segundoTermino);
 
     case "-":
-      return restar(numUno, numDos);
+      return restar(primerTermino, segundoTermino);
 
     case "*":
-      return multiplicar(numUno, numDos);
+      return multiplicar(primerTermino, segundoTermino);
 
     case "/":
-      return dividir(numUno, numDos);
+      return dividir(primerTermino, segundoTermino);
   }
 };
+
+let displayNumero = (numero) => {
+  $display.textContent += numero;
+};
+let displayResultado = (resultado) => {
+  $display.textContent = resultado;
+};
+
+let limpiarDisplay = () => {
+  $display.textContent = "";
+};
+
+let esNumero = (elemento) => {
+  return elemento.hasAttribute("data-numero") ? true : false;
+};
+let esOperador = (elemento) => {
+  return elemento.hasAttribute("data-operador") ? true : false;
+};
+let esIgualdad = (elemento) => {
+  return elemento.hasAttribute("data-igualdad") ? true : false;
+};
+let esClear = (elemento) => {
+  return elemento.hasAttribute("data-clear") ? true : false;
+};
+
+let esPosibleResolver = () => {
+  return contOperador === 1 && numero != "" && auxiliar != "" && operador != ""
+    ? true
+    : false;
+};
+
+let guardarNumero = () => {
+  auxiliar = numero;
+  numero = "";
+};
+
+let resultadoParcial = (valor) => {
+  resultado = operate(parseInt(auxiliar), parseInt(numero), operador);
+  auxiliar = resultado;
+  numero = "";
+  operador = valor;
+  contOperador = 1;
+};
+let resultadoFinal = () => {
+  return operate(parseInt(auxiliar), parseInt(numero), operador);
+};
+
+let resetear = () => {
+  numero = "";
+  auxiliar = "";
+  operador = "";
+  contOperador = 0;
+
+  resultado = "";
+};
+//Eventos
+let click = (e) => {
+  let $btn = e.target;
+  let valor = e.target.textContent;
+
+  if ($btn.classList.contains("btn")) {
+    if (esNumero($btn)) {
+      numero += valor;
+      limpiarDisplay();
+      displayNumero(numero);
+    }
+    if (esOperador($btn)) {
+      if (numero != "") {
+        if (contOperador === 0) {
+          operador = valor;
+          guardarNumero();
+          limpiarDisplay();
+          contOperador++;
+        }
+        if (esPosibleResolver()) {
+          resultadoParcial(valor);
+        }
+      }
+    }
+    if (esIgualdad($btn)) {
+      if (numero != "" && auxiliar != "" && operador != "") {
+        displayResultado(resultadoFinal());
+        resetear();
+      }
+    }
+    if (esClear($btn)) {
+      resetear();
+      limpiarDisplay();
+    }
+  }
+};
+
 //Ejecucion
-console.log(operate(4, "+", 2));
-console.log(operate(8, "-", 4));
-console.log(operate(16, "*", 8));
-console.log(operate(32, "/", 16));
+d.addEventListener("click", click);
