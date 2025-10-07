7bcb967365432c7789a435a81aca3b9a1517b3a4
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-10-07 08:33:10 -0300

    Refactorizacion del js para poder trabajar con clases

--- script.js
+++ script.js
@@ -1,127 +1,183 @@
 //Variables
 const d = document;
 
-let numero = "";
-let auxiliar = "";
-let operador = "";
-
-let contOperador = 0;
-let resultado = "";
-
-const $display = d.querySelector("[data-display]");
-
-//Funciones
-let sumar = (a, b) => a + b;
-let restar = (a, b) => a - b;
-let multiplicar = (a, b) => a * b;
-let dividir = (a, b) => a / b;
-
-let operate = (primerTermino, segundoTermino, operador) => {
-  switch (operador) {
-    case "+":
-      return sumar(primerTermino, segundoTermino);
-
-    case "-":
-      return restar(primerTermino, segundoTermino);
-
-    case "*":
-      return multiplicar(primerTermino, segundoTermino);
-
-    case "/":
-      return dividir(primerTermino, segundoTermino);
-  }
-};
-
-let displayNumero = (numero) => {
-  $display.textContent += numero;
-};
-let displayResultado = (resultado) => {
-  $display.textContent = resultado;
-};
-
-let limpiarDisplay = () => {
-  $display.textContent = "";
-};
-
-let esNumero = (elemento) => {
-  return elemento.hasAttribute("data-numero") ? true : false;
-};
-let esOperador = (elemento) => {
-  return elemento.hasAttribute("data-operador") ? true : false;
-};
-let esIgualdad = (elemento) => {
-  return elemento.hasAttribute("data-igualdad") ? true : false;
-};
-let esClear = (elemento) => {
-  return elemento.hasAttribute("data-clear") ? true : false;
-};
-
-let esPosibleResolver = () => {
-  return contOperador === 1 && numero != "" && auxiliar != "" && operador != ""
-    ? true
-    : false;
-};
-
-let guardarNumero = () => {
-  auxiliar = numero;
-  numero = "";
-};
-
-let resultadoParcial = (valor) => {
-  resultado = operate(parseInt(auxiliar), parseInt(numero), operador);
-  auxiliar = resultado;
-  numero = "";
-  operador = valor;
-  contOperador = 1;
-};
-let resultadoFinal = () => {
-  return operate(parseInt(auxiliar), parseInt(numero), operador);
-};
-
-let resetear = () => {
-  numero = "";
-  auxiliar = "";
+class Calculadora {
+  numeroUno = "";
+  numeroDos = "";
   operador = "";
-  contOperador = 0;
+  estado = true;
 
-  resultado = "";
-};
-//Eventos
-let click = (e) => {
-  let $btn = e.target;
-  let valor = e.target.textContent;
+  //Voy a crear una variable resultado
 
-  if ($btn.classList.contains("btn")) {
-    if (esNumero($btn)) {
-      numero += valor;
-      limpiarDisplay();
-      displayNumero(numero);
-    }
-    if (esOperador($btn)) {
-      if (numero != "") {
-        if (contOperador === 0) {
-          operador = valor;
-          guardarNumero();
-          limpiarDisplay();
-          contOperador++;
-        }
-        if (esPosibleResolver()) {
-          resultadoParcial(valor);
-        }
-      }
-    }
-    if (esIgualdad($btn)) {
-      if (numero != "" && auxiliar != "" && operador != "") {
-        displayResultado(resultadoFinal());
-        resetear();
-      }
-    }
-    if (esClear($btn)) {
-      resetear();
-      limpiarDisplay();
+  //Tambien deberia trabajar con decimales
+
+  get numeroUno() {
+    return this.numeroUno;
+  }
+  get numeroDos() {
+    return this.numeroDos;
+  }
+  get operador() {
+    return this.operador;
+  }
+  set numeroUno(nuevoNumero) {
+    this.numeroUno = nuevoNumero;
+  }
+  set numeroDos(nuevoNumero) {
+    this.numeroDos = nuevoNumero;
+  }
+  set operador(nuevoOperador) {
+    this.operador = nuevoOperador;
+  }
+  set estado(nuevoEstado) {
+    this.estado = nuevoEstado;
+  }
+
+  sumar(a, b) {
+    return a + b;
+  }
+  restar(a, b) {
+    return a - b;
+  }
+  multiplicar(a, b) {
+    return a * b;
+  }
+  dividir(a, b) {
+    return a / b;
+  }
+
+  calcular() {
+    switch (this.operador) {
+      case "+":
+        return this.sumar(Number(this.numeroUno), Number(this.numeroDos));
+
+      case "-":
+        return this.restar(Number(this.numeroUno), Number(this.numeroDos));
+
+      case "*":
+        return this.multiplicar(Number(this.numeroUno), Number(this.numeroDos));
+
+      case "/":
+        return this.dividir(Number(this.numeroUno), Number(this.numeroDos));
     }
   }
-};
+
+  resetear() {
+    this.numeroUno = "";
+    this.numeroDos = "";
+    this.operador = "";
+    this.estado = true;
+  }
+}
+
+class Display {
+  $display = d.querySelector("[data-display]");
+  $btnsNumeros = [...d.querySelectorAll("[data-numero]")];
+  $btnsOperadores = [...d.querySelectorAll("[data-operador]")];
+  $btnBorrar = d.querySelector("[data-clear]");
+  $btnBorrarParcial = d.querySelector("[data-aclear]");
+  $btnDecimal = d.querySelector("[data-decimal]");
+  $btnIgualdad = d.querySelector("[data-igualdad]");
+
+  render(contenido) {
+    this.$display.textContent = contenido;
+  }
+
+  limpiarDisplay() {
+    this.$display.textContent = "";
+  }
+}
+
+class Controlador {
+  controlarNumeros(elemento) {
+    if (calculadora.estado) {
+      calculadora.numeroUno += elemento;
+      display.render(calculadora.numeroUno);
+    } else {
+      calculadora.numeroDos += elemento;
+      display.render(calculadora.numeroDos);
+    }
+    console.log(calculadora);
+  }
+  controlarOperadores(operador) {
+    display.limpiarDisplay();
+    calculadora.estado = false;
+    if (calculadora.operador === "") {
+      calculadora.operador = operador;
+    } else {
+      calculadora.numeroUno = calculadora.calcular();
+      display.render(calculadora.numeroUno);
+      calculadora.numeroDos = "";
+      calculadora.operador = operador;
+    }
+  }
+
+  controlarBorrar() {
+    display.limpiarDisplay();
+    calculadora.resetear();
+    console.log(calculadora);
+  }
+
+  eliminarUltimoCaracter(numero) {
+    if (numero != "") {
+      numero = numero.slice(0, -1);
+    }
+  }
+
+  controlarBorrarParcial() {
+    if (calculadora.estado) {
+      //trabajo numero uno
+      if (calculadora.numeroUno != "") {
+        calculadora.numeroUno = calculadora.numeroUno.slice(0, -1);
+      }
+      display.render(calculadora.numeroUno);
+    } else {
+      //trabajo numero dos
+      if (calculadora.numeroDos != "") {
+        calculadora.numeroDos = calculadora.numeroDos.slice(0, -1);
+      }
+      display.render(calculadora.numeroDos);
+    }
+  }
+  controlarIgualdad() {
+    console.log(`Resultado: ${calculadora.calcular()}`);
+    if (
+      calculadora.numeroUno != "" &&
+      calculadora.numeroDos != "" &&
+      calculadora.operador != ""
+    ) {
+      display.render(calculadora.calcular());
+      calculadora.resetear();
+    }
+  }
+
+  click(e) {
+    if (e.target.classList.contains("btn")) {
+      if (display.$btnsNumeros.includes(e.target)) {
+        controlador.controlarNumeros(e.target.textContent);
+      }
+      if (display.$btnsOperadores.includes(e.target)) {
+        //me falta controlar que presiones 2 veces el operador
+        if (calculadora.numeroUno != "") {
+          controlador.controlarOperadores(e.target.textContent);
+        }
+      }
+      if (display.$btnIgualdad === e.target) {
+        controlador.controlarIgualdad();
+      }
+      if (display.$btnBorrar === e.target) {
+        controlador.controlarBorrar();
+      }
+      if (display.$btnBorrarParcial === e.target) {
+        controlador.controlarBorrarParcial();
+      }
+    }
+  }
+}
+
+const calculadora = new Calculadora();
+const display = new Display();
+const controlador = new Controlador();
 
 //Ejecucion
-d.addEventListener("click", click);
+d.addEventListener("click", controlador.click);
